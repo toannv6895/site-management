f2bb19ce8aa07a1bcf79ec127b38f99ac8f0c7c4
Toan Nguyen <tnguyen@us.maxnerva.com>
2024-03-24 17:55:14 +0700

    init project

---
+++ src/test/java/org/toannguyen/SiteManagementIT.java
@@ -0,0 +1,8 @@
+package org.toannguyen;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class SiteManagementIT extends SiteManagementTest {
+    // Execute the same tests but in packaged mode.
+}
